e47b3c4ee174ec45160e6cc9f3fbec42ce78b771
felipesora <felipeusora@gmail.com>
2025-11-12 15:13:40 -0300

    wip: criando dto e service de Trilha

---
+++ NS.Application/Dtos/TrilhaDTO.cs
@@ -0,0 +1,26 @@
+﻿using NS.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace NS.Application.Dtos;
+
+public class TrilhaDTO
+{
+    [Required]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Descricao { get; set; }
+
+    [Required]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public AreaTrilha Area { get; set; }
+
+    [Required]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public NivelTrilha Nivel { get; set; }
+
+    [Required]
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public StatusTrilha Status { get; set; }
+}
---
+++ NS.Application/Interfaces/ITrilhaService.cs
@@ -0,0 +1,10 @@
+﻿using NS.Domain.Entities;
+
+namespace NS.Application.Interfaces;
+
+public interface ITrilhaService
+{
+    Task<OperationResult<PageResultModel<IEnumerable<TrilhaEntity>>>> ObterTodasTrilhasAsync(int deslocamento = 0, int registrosRetornados = 10);
+    Task<OperationResult<PageResultModel<IEnumerable<TrilhaEntity>>>> ObterTrilhasAtivasAsync(int deslocamento = 0, int registrosRetornados = 10);
+    Task<OperationResult<TrilhaEntity?>> ObterTrilhaPorIdAsync(long id);
+}
