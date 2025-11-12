2561683d012a5f4c1771149c56c51a8a3a31cb96
felipesora <felipeusora@gmail.com>
2025-11-12 16:06:58 -0300

    feat: Finalizando o controller de Trilha

---
+++ NS.Infra.IoC/Bootstrap.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using HealthChecks.Oracle;
+using NS.Infra.Data.AppData;
+using NS.Domain.Interfaces;
+using NS.Infra.Data.Repositories;
+using NS.Application.Interfaces;
+using NS.Application.Services;
+
+namespace NS.Infra.IoC;
+
+public class Bootstrap
+{
+    public static void AddIoC(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationContext>(options => {
+            //options.UseOracle(configuration.GetConnectionString("Oracle"));
+            options.UseOracle(configuration.GetConnectionString("Oracle"));
+        });
+
+        //services.AddHealthChecks()
+        //    // Liveness: verifica api “estou no ar”
+        //    .AddCheck("self", () => HealthCheckResult.Healthy(), tags: new[] { "live" })
+        //    //Readiness: verifica se o mongo "esta online"
+        //    .AddCheck<OracleHealthCheck>("oracle_ef_query", tags: new[] { "ready" });
+
+        services.AddTransient<ITrilhaRepository, TrilhaRepository>();
+        services.AddTransient<ITrilhaService, TrilhaService>();
+    }
+}
---
+++ NS.Presentation/Doc/Samples/Trilha/TrilhaResponseSample.cs
@@ -0,0 +1,22 @@
+﻿using NS.Domain.Entities;
+using NS.Domain.Enums;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace NS.Presentation.Doc.Samples.Trilha;
+
+public class TrilhaResponseSample : IExamplesProvider<TrilhaEntity>
+{
+    public TrilhaEntity GetExamples()
+    {
+        return new TrilhaEntity()
+        {
+            Id = 1,
+            Nome = "Backend com Java e Spring Boot",
+            Descricao = "Aprenda a desenvolver APIs REST robustas com Java e Spring Boot, integrando banco de dados, segurança e boas práticas de arquitetura",
+            Area = AreaTrilha.BACKEND,
+            Nivel = NivelTrilha.INTERMEDIARIO,
+            Status = StatusTrilha.ATIVA,
+            DataCriacao = DateTime.Parse("12/11/2025 15:17:15")
+        };
+    }
+}
