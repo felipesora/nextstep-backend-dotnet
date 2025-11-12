bb7fd697923557be52aea62cdf1820159f718452
felipesora <felipeusora@gmail.com>
2025-11-12 14:59:49 -0300

    wip: criando repositório de Trilha

---
+++ NS.Infra.Data/AppData/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NS.Domain.Entities;
+
+namespace NS.Infra.Data.AppData;
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TrilhaEntity>(entity =>
+        {
+            entity.Property(e => e.Area).HasConversion<string>();
+            entity.Property(e => e.Nivel).HasConversion<string>();
+            entity.Property(e => e.Status).HasConversion<string>();
+        });
+    }
+
+    public DbSet<TrilhaEntity> Trilha { get; set; }
+}
