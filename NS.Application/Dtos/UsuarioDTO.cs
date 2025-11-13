625b7efbeb22787128460c2513a02cdb78c88182
felipesora <felipeusora@gmail.com>
2025-11-13 15:40:11 -0300

    wip: criando dto, mapper e service de Usuario

---
+++ NS.Application/Dtos/UsuarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NS.Application.Dtos;
+
+public class UsuarioDTO
+{
+    [Required]
+    public string Nome { get; set; }
+
+    [Required]
+    public string Email { get; set; }
+
+    [Required]
+    public string Senha { get; set; }
+}
