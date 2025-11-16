7c14802cee5a33d10f00c935c56a9b8ee7cc4d83
felipesora <felipeusora@gmail.com>
2025-11-16 01:46:20 -0300

    wip: criando entidade de formulario

---
+++ NS.Domain/Enums/AreaTecnologia.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NS.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum AreaTecnologia
+{
+    FRONTEND,
+    BACKEND,
+    MOBILE,
+    CLOUD,
+    DADOS,
+    CIBER,
+    DESIGN
+}
---
+++ NS.Domain/Enums/ObjetivoCarreira.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace NS.Domain.Enums;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ObjetivoCarreira
+{
+    PRIMEIRO_EMPREGO,
+    MUDAR_CARREIRA,
+    CRESCER_AREA,
+    APRENDER
+}
